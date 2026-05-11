fe94ff06965a9e88752f8bcda92ac77d3e82f318
VAdityaRao <118269699+VAdityaRao@users.noreply.github.com>
2026-05-11 02:06:57 -0500

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+
+        stack <int> s;
+
+        for (int i = 0; i < operations.size(); i++){
+            if (operations[i] == "C"){
+                s.pop();
+            }
+
+            else if (operations[i]  == "D"){
+                s.push(2 * s.top());
+            }
+
+            else if (operations[i]  == "+"){
+                int x = s.top();
+                s.pop();
+                int y = s.top();
+                s.push (x);
+                s.push (x+y);
+            }
+
+            else {
+                s.push (stoi(operations[i]));
+            }
+
+        }
+
+        int sum = 0;
+        while (!s.empty()){
+            sum += s.top();
+            s.pop();
+        }
+
+        return sum;
+        
+    }
+};
